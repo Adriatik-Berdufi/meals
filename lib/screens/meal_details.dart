e5464392390bd89416b2f7d0fe28a2d62b9d34ce
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-10-05 17:22:14 +0200

    print all meal details

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -10,11 +10,55 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: ListView(
+        children: [
+          Image.network(
+            meal.imageUrl,
+            width: double.infinity,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 12),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 16),
+          for (final ingridient in meal.ingredients)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                '-$ingridient',
+                textAlign: TextAlign.start,
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+              ),
+            ),
+          const SizedBox(height: 24),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 12),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Text(
+                textAlign: TextAlign.start,
+                '-$step',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+              ),
+            ),
+        ],
       ),
     );
   }
